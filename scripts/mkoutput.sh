546e5fe33635d7d624f4a8c90e08fb290e6c0464
Jennifer Hodgdon <yahgrp@poplarware.com>
2015-07-30 13:54:27 -0700

    Issue #2508001 by jhodgdon: Make better e-book output

--- scripts/mkoutput.sh
+++ scripts/mkoutput.sh
@@ -13,6 +13,7 @@ mkdir -p ../output/ebooks
 mkdir -p ../output/ebooks/en
 mkdir -p ../output/ebooks/en/images
 
+
 # Run the preprocessor that puts file names into the files under each header.
 # Only for the special HTML output.
 php addnames._php ../source/en ../output/html/en
@@ -30,14 +31,14 @@ asciidoc -d book -b docbook -f std.conf -o ../output/html/guidelines/guidelines.
 xmlto -m bare.xsl xhtml -o ../output/html/en ../output/html/en/guide.docbook
 xmlto -m bare.xsl xhtml -o ../output/html/guidelines ../output/html/guidelines/guidelines.docbook
 
-# Copy image files to output directory
+# Copy image files to output directory.
 cp ../source/en/images/*.png ../output/html/en/images
 cp ../guidelines/images/*.png ../output/html/guidelines/images
 
 # Run the AsciiDoc processor to convert to DocBook for ebooks.
 asciidoc -d book -b docbook -f std.conf -o ../output/ebooks/en/guide.docbook ../source/en/guide.txt
 
-# Copy image files to e-book directory
+# Copy image files to e-book directory.
 cp ../source/en/images/*.png ../output/ebooks/en/images
 
 # Run the xmlto processor to convert from DocBook to PDF.
@@ -48,11 +49,21 @@ xmlto pdf  -m pdf.xsl --with-fop -o ../output/ebooks/en ../output/ebooks/en/guid
 # Run the xmlto processor to convert from DocBook to ePub.
 # The syntax is:
 #   xmlto epub -o [output dir] [input docbook file]
-xmlto epub  -m epub.xsl -o ../output/ebooks/en ../output/ebooks/en/guide.docbook
+# And we need to do this for the regular and mobi styles.
+xmlto epub -m epub.xsl -o ../output/ebooks/en ../output/ebooks/en/guide.docbook
+cp ../output/ebooks/en/guide.docbook ../output/ebooks/en/guide-simple.docbook
+xmlto epub -m mobi.xsl -o ../output/ebooks/en ../output/ebooks/en/guide-simple.docbook
+
+# At this point, we need to CD to the output directory to do some more
+# processing, to add images to the epub files.
+cd ../output/ebooks/en
+mkdir -p OEBPS
+mkdir -p OEBPS/images
+cp images/* OEBPS/images
+zip guide.epub OEBPS/images/*
+zip guide-simple.epub OEBPS/images/*
 
 # Run the calibre processor to convert from ePub to Mobi, but on a modified
 # ePub format. The syntax is:
 #   ebook-convert [input epub file] [output file] [options]
-cp ../output/ebooks/en/guide.docbook ../output/ebooks/en/guide-simple.docbook
-xmlto -m mobi.xsl epub -o ../output/ebooks/en ../output/ebooks/en/guide-simple.docbook
-ebook-convert ../output/ebooks/en/guide-simple.epub ../output/ebooks/en/guide.mobi
+ebook-convert guide-simple.epub guide.mobi
