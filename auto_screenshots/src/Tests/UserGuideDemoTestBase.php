25967072061072899c9d742476c6d571072f5a4f
Jennifer Hodgdon <yahgrp@poplarware.com>
2016-12-05 16:28:49 -0800

    Issue #2830624 by jhodgdon: Add a batch of Simplified Chinese screenshots

--- auto_screenshots/src/Tests/UserGuideDemoTestBase.php
+++ auto_screenshots/src/Tests/UserGuideDemoTestBase.php
@@ -38,7 +38,7 @@ require __DIR__ . '/../../vendor/autoload.php';
  *   column A, and the translated text in column C, you can use this formula
  *   to generate the array in row 2 (and then copy to the other rows):
  *   =IF(A2 <> "","'"&A2&"' => """&C2&""",","")
- *   Then just copy this column of output into the $deomInput array in your
+ *   Then just copy this column of output into the $demoInput array in your
  *   new class.
  * - Override the $runList member variable to run the sections of interest.
  *
