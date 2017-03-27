aa8af45cbab0f3bf4013ab4d011a0cd7062e70e9
Jennifer Hodgdon <yahgrp@poplarware.com>
2017-03-27 15:36:16 -0700

    Issue #2782897 by jhodgdon: Add UI text tests for the Blocks chapter

--- auto_screenshots/src/Tests/UserGuideDemoTestEn.php
+++ auto_screenshots/src/Tests/UserGuideDemoTestEn.php
@@ -20,11 +20,11 @@ class UserGuideDemoTestEn extends UserGuideDemoTestBase {
    * change 'skip' to one of the other values for each chapter you want to run.
    * See UserGuideDemoTestBase::runList for more information.
    */
-  protected $runList = [
+  protected $notRunList = [
     'doPrefaceInstall' => 'skip',
-    'doBasicConfig' => 'restore',
-    'doBasicPage' => 'run',
-    'doContentStructure' => 'run',
+    'doBasicConfig' => 'skip',
+    'doBasicPage' => 'skip',
+    'doContentStructure' => 'skip',
     'doUserAccounts' => 'skip',
     'doBlocks' => 'skip',
     'doViews' => 'skip',
--- auto_screenshots/src/Tests/UserGuideDemoTestHu.php
+++ auto_screenshots/src/Tests/UserGuideDemoTestHu.php
@@ -20,12 +20,12 @@ class UserGuideDemoTestHu extends UserGuideDemoTestBase {
    * change 'skip' to one of the other values for each chapter you want to run.
    * See UserGuideDemoTestBase::runList for more information.
    */
-  protected $runList = [
+  protected $notRunList = [
     'doPrefaceInstall' => 'skip',
     'doBasicConfig' => 'skip',
     'doBasicPage' => 'skip',
     'doContentStructure' => 'skip',
-    'doUserAccounts' => 'restore',
+    'doUserAccounts' => 'skip',
     'doBlocks' => 'skip',
     'doViews' => 'skip',
     'doMultilingualSetup' => 'skip',
